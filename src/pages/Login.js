d7cc8e9fbeccf62f64bf2bc3034d5bcffb94d7d6
maftuhlutfi <playmakernyomlo@gmail.com>
2020-08-03 16:12:36 +0700

    replace class component state with function component and useState

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Login.scss';
 import TextField from '../components/TextField';
 import CustomButton from '../components/CustomButton';
@@ -8,76 +8,65 @@ import GoogleButton from '../components/GoogleButton';
 import { connect } from 'react-redux';
 import { googleSignInStart, emailSignInStart } from '../redux/actions/userActions';
 
-class Login extends React.Component {
-	constructor() {
-		super();
+const Login = ({ emailSignInStart, googleSignInStart }) => {
+	const [input, setInput] = useState({
+		email: '',
+		password: ''
+	})
+	const { email, password } = input;
 
-		this.state = {
-			email: '',
-			password: '',
-			error: ''
-		}
-	}
+	const [error, setError] = useState('');
 
-	handleChange = e => {
-		this.setState({ error: '' });
+	const handleChange = e => {
 		const { value, name } = e.target;
-
-		this.setState({ [name]: value });
+		setInput({ ...input, [name]: value });
+		setError('');
 	}
 
-	handleClick = async e => {
+	const handleClick = async e => {
 		e.preventDefault();
 
-		const { email, password } = this.state;
-		const { emailSignInStart } = this.props;
-
 		if (email === '' || password === '') {
-			this.setState({ error: 'Please enter email or password' });
+			setError('Please enter email or password');
 			return;
 		}
 
 		emailSignInStart(email, password);
 	}
 
-	render() {
-		const { email, password, error } = this.state;
-		const { googleSignInStart } = this.props;
-
-		return (
-			<div className='login'>
-				<h2>Login</h2>
-				<p>Please login with your email and password</p>
-				<form className='form'>
-					<TextField
-						name='email'
-						onChange={this.handleChange}
-						value={email}
-						type='text'
-						placeholder='Email'
-						required
-					/>
-					<TextField
-						name='password'
-						onChange={this.handleChange}
-						value={password}
-						type='password'
-						placeholder='Password'
-						required
-					/>
-				</form>
-				{error !== '' && <p className='error'>{error}</p>}
-				<CustomButton onClick={this.handleClick}>Login</CustomButton>
-				<GoogleButton onClick={googleSignInStart}>Sign in with Google</GoogleButton>
-				<p className='footer'>Don't have an account yet? <Link to='/register'>Register here</Link></p>
-			</div>
-		);
-	}
+	return (
+		<div className='login'>
+			<h2>Login</h2>
+			<p>Please login with your email and password</p>
+			<form className='form'>
+				<TextField
+					name='email'
+					onChange={handleChange}
+					value={email}
+					type='text'
+					placeholder='Email'
+					required
+				/>
+				<TextField
+					name='password'
+					onChange={handleChange}
+					value={password}
+					type='password'
+					placeholder='Password'
+					required
+				/>
+			</form>
+			{error !== '' && <p className='error'>{error}</p>}
+			<CustomButton onClick={handleClick}>Login</CustomButton>
+			<GoogleButton onClick={googleSignInStart}>Sign in with Google</GoogleButton>
+			<p className='footer'>Don't have an account yet? <Link to='/register'>Register here</Link></p>
+		</div>
+	);
 }
 
 const mapDispatchToProps = dispatch => ({
 	googleSignInStart: () => dispatch(googleSignInStart()),
-	emailSignInStart: (email, password) => dispatch(emailSignInStart({email, password}))
+	emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password }))
 })
 
 export default connect(null, mapDispatchToProps)(Login);
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,97 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.scss';
 import { Link } from 'react-router-dom';
 import TextField from '../components/TextField';
 import CustomButton from '../components/CustomButton';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { signUpStart } from '../redux/actions/userActions';
 
-class Register extends React.Component {
-	constructor() {
-		super();
+const Register = ({ signUpStart }) => {
+	const [input, setInput] = useState({
+		displayName: '',
+		email: '',
+		password: '',
+		confirmPassword: '',
+	})
+	const { displayName, email, password, confirmPassword } = input;
 
-		this.state = {
-			displayName: '',
-			email: '',
-			password: '',
-			confirmPassword: '',
-			error: ''
-		}
-	}
+	const [error, setError] = useState('');
 
-	handleChange = e => {
-		this.setState({ error: '' });
+	const handleChange = e => {
 		const { value, name } = e.target;
-
-		this.setState({ [name]: value });
+		setInput({ ...input, [name]: value });
+		setError('');
 	}
 
-	handleClick = e => {
+	const handleClick = e => {
 		e.preventDefault();
 
-		const { displayName, email, password, confirmPassword } = this.state;
-		const { signUpStart } = this.props;
-
-		if (displayName === '' || email ==='' || password === '') {
-			this.setState({ error: 'Please fill the form.' });
+		if (displayName === '' || email === '' || password === '') {
+			setError('Please fill the form.');
 			return;
 		}
 
 		if (password !== confirmPassword) {
-			this.setState({ error: 'Password not match.' });
+			setError('Password not match.');
 			return;
 		}
 
 		signUpStart({ email, password, displayName });
 	}
 
-	render() {
-		const { displayName, email, password, confirmPassword, error } = this.state;
-		return (
-			<div className='login'>
-				<h2>Register</h2>
-				<p>To find amazing clothes for your Outfit of the Day</p>
-				<form className='form'>
-					<TextField
-						name='displayName'
-						onChange={this.handleChange}
-						value={displayName}
-						type='text'
-						placeholder='Display Name'
-						required
-					/>
-					<TextField
-						name='email'
-						onChange={this.handleChange}
-						value={email}
-						type='text'
-						placeholder='Email'
-						required
-					/>
-					<TextField
-						name='password'
-						onChange={this.handleChange}
-						value={password}
-						type='password'
-						placeholder='Password'
-						required
-					/>
-					<TextField
-						name='confirmPassword'
-						onChange={this.handleChange}
-						value={confirmPassword}
-						type='password'
-						placeholder='Confirm Password'
-						required
-					/>
-				</form>
-				{error !== '' && <p className='error'>{error}</p>}
-				<CustomButton onClick={this.handleClick}>Register</CustomButton>
-				<p className='footer'>Already have an account? <Link to='/login'>Login here</Link></p>
-			</div>
-		);
-	}
+	return (
+		<div className='login'>
+			<h2>Register</h2>
+			<p>To find amazing clothes for your Outfit of the Day</p>
+			<form className='form'>
+				<TextField
+					name='displayName'
+					onChange={handleChange}
+					value={displayName}
+					type='text'
+					placeholder='Display Name'
+					required
+				/>
+				<TextField
+					name='email'
+					onChange={handleChange}
+					value={email}
+					type='text'
+					placeholder='Email'
+					required
+				/>
+				<TextField
+					name='password'
+					onChange={handleChange}
+					value={password}
+					type='password'
+					placeholder='Password'
+					required
+				/>
+				<TextField
+					name='confirmPassword'
+					onChange={handleChange}
+					value={confirmPassword}
+					type='password'
+					placeholder='Confirm Password'
+					required
+				/>
+			</form>
+			{error !== '' && <p className='error'>{error}</p>}
+			<CustomButton onClick={handleClick}>Register</CustomButton>
+			<p className='footer'>Already have an account? <Link to='/login'>Login here</Link></p>
+		</div>
+	);
 }
 
 const mapDispatchToProps = dispatch => ({
